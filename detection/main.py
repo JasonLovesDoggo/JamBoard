8d9cd985fc2518ac73e633d3b79abca244c61189
Jason Cameron <git@jasoncameron.dev>
2024-08-04 06:57:16 -0400

    recalculate shapes on starteup twice

--- detection/main.py
+++ detection/main.py
@@ -7,7 +7,14 @@ from .utils import (
     calibrate,
     initialize_video_captures,
     check_video_capture,
+    shapes_objects
 )
+import pygame
+from .sound_main import populate_shapes_formatted
+from .fuck import create_pairs
+from .calculations import (find_nearest_line_to_finger_tip, create_bounding_box_centers, check_if_finger_tip_is_in_bounding_box)
+import time
+
 from .tapping import is_tapped
 
 mp_hands = mp.solutions.hands
@@ -27,11 +34,29 @@ def main_loop(cap_top, cap_side, hands, drawing_utils, paper_roi):
             cv2.destroyAllWindows()
             exit()
         calibrated_shapes = calibrate(frame, paper_roi)
+    _, frame = cap_top.read()
+    calibrated_shapes = calibrate(frame, paper_roi, cap_side=cap_side)
 
     tapped = False  # Flag to track tapping state
     current_finger_position = (
         None  # Variable to store current position of the index finger
     )
+
+    # MINGLUNS MESS OF CODE
+    shapes_formatted = populate_shapes_formatted(shapes_objects)
+
+    bounding_box_size = 75
+    num_bounding_boxes = 8
+    # shape_steps = [0 for i in range(4)]
+
+    pygame.mixer.pre_init(frequency=16000, channels=1, allowedchanges=0)
+    pygame.init()
+    pygame.mixer.init()
+
+    pairs_dict = {}
+    for shape in shapes_formatted.keys():
+        pairs = create_pairs(shapes_formatted[shape], num_bounding_boxes, shape)
+        pairs_dict[shape] = pairs
 
     while cap_top.isOpened() and cap_side.isOpened():
         success, image = cap_top.read()
@@ -46,19 +71,57 @@ def main_loop(cap_top, cap_side, hands, drawing_utils, paper_roi):
         results = hands.process(image)
         # Continuously update the current position of the index finger
         if results.multi_hand_landmarks:
-            print(len(results.multi_hand_landmarks))
             index_finger_tip = results.multi_hand_landmarks[0].landmark[
-                    mp_hands.HandLandmark.INDEX_FINGER_TIP
-                ]
+                mp_hands.HandLandmark.INDEX_FINGER_TIP
+            ]
             current_finger_position = (
-                    index_finger_tip.x * image.shape[1],
-                    index_finger_tip.y * image.shape[0],
-                )
+                index_finger_tip.x * image.shape[1],
+                index_finger_tip.y * image.shape[0],
+            )
             print(f"Current finger position: {current_finger_position}")
         if is_tapped(cap_side):
             if CURRENT_OBJECT is not None:  # Ensure there is a current object
                 if not tapped:
                     print(f"Tapped on: {CURRENT_OBJECT}")
+
+                    # MINGLUN DO SHII
+
+                    starting_note_info = CURRENT_OBJECT
+                    if not current_finger_position:
+                        current_finger_position = (459, 255)
+                    finger_tip = current_finger_position
+
+                    nearest_line_connection = None
+                    if len(shapes_formatted[starting_note_info.name]) > 1:
+                        points = []
+                        for i in shapes_formatted[starting_note_info.name]:
+                            points.append(i["center"])
+                        nearest_line_connection = find_nearest_line_to_finger_tip(
+                            finger_tip, points, starting_note_info
+                        )
+                        bounding_box_centers = create_bounding_box_centers(
+                            starting_note_info.center, nearest_line_connection, num_bounding_boxes
+                        )
+
+                        selected_bounding_box_index = 0
+                        for i, bounding_box in enumerate(bounding_box_centers):
+                            if check_if_finger_tip_is_in_bounding_box(
+                                finger_tip, bounding_box, bounding_box_size
+                            ):
+                                selected_bounding_box_index = i
+                                break
+                    else:
+                        selected_bounding_box_index = 0
+
+                    if selected_bounding_box_index is None:
+                        print("Finger tip is not in bounding box")
+                    else:
+                        if nearest_line_connection != None:
+                            channel = pairs_dict[starting_note_info.name][starting_note_info.center + nearest_line_connection][selected_bounding_box_index].play()
+                        else:
+                            channel = pairs_dict[starting_note_info.name][starting_note_info.center + starting_note_info.center][selected_bounding_box_index].play()
+                    # MINGLUN DO SHII
+
                     tapped = True  # Set the flag to indicate tapping detected
         else:
             tapped = False  # Reset the flag when tapping is no longer detected
@@ -71,7 +134,9 @@ def main_loop(cap_top, cap_side, hands, drawing_utils, paper_roi):
         elif key == ord("c"):
             print("Recalibrating...")
             _, frame = cap_top.read()
-            calibrated_shapes = calibrate(frame, paper_roi, cap_side=cap_side)
+            calibrated_shapes = calibrate(
+                frame, paper_roi, cap_side=cap_side
+            )
 
 
 def start():
